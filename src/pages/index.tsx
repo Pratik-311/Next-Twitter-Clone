683f4947fb753c48af769167b931a989105cf88f
Pratik Pathak <pratik.pathak3nov@gmail.com>
2023-04-01 05:12:55 +0530

    Router, tRPC

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,8 +6,8 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
   const user = useUser();
+  const { data } = api.posts.getAll.useQuery();
   return (
     <>
       <Head>
@@ -16,9 +16,13 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-          <div>
+          <div style={{color: "beige"}}>
               {!user.isSignedIn ? <SignInButton /> : <SignOutButton />}
               <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+          </div>
+          <div>
+            {data?.map((post) => 
+                (<div key={post.id}>{post.content}</div>))}
           </div>
       </main>
     </>
